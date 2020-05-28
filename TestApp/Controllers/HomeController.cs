7040fbd234172cb97c224c741a5d3d12e2ed7f86
s.hirano6022 <66051423+hirano6022@users.noreply.github.com>
2020-05-29 00:11:19 +0900

    Git Test (Add comment 2)

--- TestApp/Controllers/HomeController.cs
+++ TestApp/Controllers/HomeController.cs
@@ -1,23 +1,51 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Configuration;
+using TestApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
+
+// Git test
 
 namespace TestApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly TelemetryClient telemetry = new TelemetryClient();
+
         public ActionResult Index()
         {
+            telemetry.TrackEvent("Index Requested");
+
+            var conn = ConfigurationManager.AppSettings["Storage"];
+            var account = CloudStorageAccount.Parse(conn);
+            var client = account.CreateCloudTableClient();
+            var table = client.GetTableReference("accesslog");
+            table.CreateIfNotExists();
+            var msg = new AccessLog() { LogMessage = $"{nameof(Index)} GET でアクセスしました " };
+            var insert = TableOperation.Insert(msg);
+            table.Execute(insert);
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            try
+            {
+                throw new NotImplementedException("Exception の実験");
+                ViewBag.Message = "Your application description page.";
+                return View();
+            }
+            catch(Exception ex)
+            {
+                telemetry.TrackException(ex);
+                throw;
+            }
+            
         }
 
         public ActionResult Contact()
---
+++ TestApp/Models/AccessLog.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApp.Models
+{
+    public class AccessLog : TableEntity
+    {
+        public DateTime LogDateTime { get; set; }
+        public string LogMessage { get; set; }
+
+        public AccessLog()
+        {
+            PartitionKey = nameof(AccessLog);
+            RowKey = Guid.NewGuid().ToString();
+            LogDateTime = DateTime.UtcNow;
+        }
+
+    }
+}
